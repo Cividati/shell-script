c5486e6f59994a071187d622063e0dd949bc3f37
Rubens Cividati <rubens.cividati.teles@gmail.com>
2020-02-11 01:12:19 -0300

    Create convert-jpg-png.sh

---
+++ convert-jpg-png.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+convert_imagem(){
+	cd ~/downloads/imagens-livros
+	if [ ! -d png ]
+	then
+		mkdir png
+	fi
+
+	for imagem in *.jpg
+	do
+		local imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1}')
+		convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
+	done
+}
+
+converte_imagem 2>erros_conversao.txt
+if [ $? -eq 0 ]
+then
+	echo "Conversao realizada com sucesso"
+else
+	echo "Houve uma falha no processo"
+fi
